e3bea7f59242cedd0f98bc1e11be479ca0206ae8
Benjamin Fradet <benjamin.fradet@gmail.com>
2017-04-08 16:23:41 +0100

    spec

---
+++ src/test/scala/benfradet/ste/StructTypeEncoderSpec.scala
@@ -0,0 +1,42 @@
+package benfradet.ste
+
+import org.apache.spark.sql.types._
+import org.scalatest.{FlatSpec, Matchers}
+
+class StructTypeEncoderSpec extends FlatSpec with Matchers {
+  import StructTypeEncoder._
+
+  "A StructTypeEncoder" should "deal with the supported primitive types" in {
+    case class Foo(a: Boolean, b: Byte, c: Double, d: Float, e: Int, f: Long, g: Short, h: String)
+    StructTypeEncoder[Foo].encode shouldBe StructType(
+      StructField("a", BooleanType) ::
+      StructField("b", ByteType) ::
+      StructField("c", DoubleType) ::
+      StructField("d", FloatType) ::
+      StructField("e", IntegerType) ::
+      StructField("f", LongType) ::
+      StructField("g", ShortType) ::
+      StructField("h", StringType) :: Nil
+    )
+  }
+
+  it should "deal with the supported combinators" in {
+    case class Foo(a: Array[Int], b: List[Int], c: Set[Int], d: Vector[Int], e: Map[String, Int])
+    StructTypeEncoder[Foo].encode shouldBe StructType(
+      StructField("a", ArrayType(IntegerType)) ::
+      StructField("b", ArrayType(IntegerType)) ::
+      StructField("c", ArrayType(IntegerType)) ::
+      StructField("d", ArrayType(IntegerType)) ::
+      StructField("e", MapType(StringType, IntegerType)) :: Nil
+    )
+  }
+
+  it should "deal with nested products" in {
+    case class Foo(a: Int)
+    case class Bar(f: Foo, b: Int)
+    StructTypeEncoder[Bar].encode shouldBe StructType(
+      StructField("f", StructType(StructField("a", IntegerType) :: Nil)) ::
+      StructField("b", IntegerType) :: Nil
+    )
+  }
+}
